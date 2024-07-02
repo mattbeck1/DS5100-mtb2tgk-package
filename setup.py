0efc5e601e3b6dfdc889ddd0de2cb9411983795a
Matthew Beck <mtb2tgk@virginia.edu>
2024-07-02 11:04:29 -0400

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'DS5100-mtb2tgk-package',
+    name = 'Wahoowa',
     version = '1.0.0',
     author = 'Matthew Beck',
     author_email = 'mtb2tgk@virginia.edu',
@@ -9,6 +9,4 @@ setup(
     url = 'https://github.com/mattbeck1/DS5100-mtb2tgk-package',
     license = 'LICENSE.txt',
     description = 'A package to celebrate a UVA win',
-    long_description = open('README.txt').read(),
-    install_requires = [],
 )
